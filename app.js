a7023aff1c40add95f79b9b62a9de7b2dab7cd86
matthewdjoseph <matthewjoseph.student@careerdevs.com>
2019-09-05 18:33:03 -0400

    fixed version

--- app.js
+++ app.js
@@ -1,11 +1,72 @@
-// video 4
+// video 18
 
-const wmf = document.querySelector('#book-list li:nth-child(2) .name');
+document.addEventListener('DOMContentLoaded', function(){
+    const list = document.querySelector('#book-list ul');
 
-var books = document.querySelector('#book-list li .name');
+    list.addEventListener('click', function(e){
+        if(e.target.className == 'delete'){
+            const li = e.target.parentElement;
+            list.removeChild(li);
+        }
+    });
 
-books = document.querySelectorAll('#book-list li .name');
+    const addForm = document.forms['add-book'];
+    addForm.addEventListener('submit', function(e){
+        e.preventDefault();
+        const value = addForm.querySelector('input[type="text"]').value;
 
-Array.from(books).forEach(function(book){
-    console.log(book);
-})
+        const li = document.createElement('li');
+        const bookName = document.createElement('span');
+        const deleteBtn = document.createElement('span');
+
+        deleteBtn.textContent = 'delete';
+        bookName.textContent = value;
+
+        bookName.classList.add('name');
+        deleteBtn.classList.add('delete');
+
+        li.appendChild(bookName);
+        li.appendChild(dleteBtn);
+        li.appendChild(li);
+
+    });
+
+    const hideBox = document.querySelector('#hide');
+    hideBox.addEventListener('change', function(e){
+        if(hideBox.checked){
+            list.style.display = "none";
+        }else{
+            list.style.display = "initial";
+        }
+    });
+
+    const searchBar = document.forms['search-books'].querySelector('input');
+    searchBar.addEventListener('keyup', function(e){
+        const term = e.target.value.toLowerCase();
+        const books = list.getElementsByTagName('li');
+        Array.from(books).forEach(function(book){
+            const title = book.firstElementChild.textContent;
+            if(title.toLowerCase().indexOf(term) != -1){
+                book.style.display = 'block';
+            }else{
+                book.style.display = 'none';
+            }
+        })
+    })
+
+    const tabs = document.querySelector('.tabs');
+    const panels = document.querySelectorAll('.panel');
+    tabs.addEventListener('click', function(e){
+        if(e.target.tagName == "LI"){
+            const targetPanel = document.querySelector(e.target.dataset.target);
+            panels.forEach(function(panel){
+                if(panel == targetPanel){
+                    panel.classList.add('active');
+                }else{
+                    panel.classList.remove('active');
+                }
+            });
+        }
+    });
+});
+
