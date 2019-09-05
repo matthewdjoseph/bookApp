2031ecdfc8e09577e018136721b2691c4308e5b2
matthewdjoseph <matthewjoseph.student@careerdevs.com>
2019-09-04 21:13:33 -0400

    showing siblings in js

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
-// video 7
+// video 8
 
 const bookList = document.querySelector('#book-list');
 
-console.log('the parent node is:', bookList.parentNode);
-console.log('the parent element is:', bookList.parentElement.parentElement);
+console.log('book-list next sibling is:', bookList.nextSibling);
+console.log('book-list next element sibling is:', bookList.nextElementSibling);
 
-console.log(bookList.children);
+console.log('book-list previous sibling is:', bookList.previousSibling);
+console.log('book-list previous element sibling is:', bookList.previousElementSibling);
+
+bookList.previousElementSibling.querySelector('p').innerHTML += '<br />Too cool for everyone else!';
