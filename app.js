022556022d8c5ff551a683caf65eac7681d82dad
matthewdjoseph <matthewjoseph.student@careerdevs.com>
2019-09-04 21:25:00 -0400

    added add listener

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// video 10
+// video 11
 
 const list = document.querySelector('#book-list ul');
 
@@ -7,4 +7,12 @@ list.addEventListener('click', function(e){
         const li = e.target.parentElement;
         list.removeChild(li);
     }
+});
+
+const addForm = document.forms['add-book'];
+
+addForm.addEventListener('submit', function(e){
+    e.preventDefault();
+    const value = addForm.querySelector('input[type="text"]').value;
+    console.log(value);
 });
