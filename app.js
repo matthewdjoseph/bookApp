dd7dafe66fc2aaac444063cf9adbe340204b9f99
matthewdjoseph <matthewjoseph.student@careerdevs.com>
2019-09-04 21:05:44 -0400

    displaying node properties

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
-// video 5
+// video 6
 
-var books = document.querySelectorAll('#book-list li .name');
+const banner = document.querySelector('#page-banner');
 
-Array.from(books).forEach(function(book){
-    book.textContent += ' (book title)';
-})
+console.log('#page-banner node type is:', banner.nodeType);
+console.log('#page-banner node name is:', banner.nodeName);
+console.log('#page-banner has child nodes:', banner.hasChildNodes());
 
-const bookList = document.querySelector('#book-list');
-
-bookList.innerHTML += '<p>This is how you add HTML</p>';
+const clonedBanner = banner.cloneNode(true);
+console.log(clonedBanner);
